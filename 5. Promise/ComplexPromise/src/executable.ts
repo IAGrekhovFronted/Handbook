51dfec0c25d5859e9e9eefe05636acaa720e6641
IAGrekhovFronted <iagrekhov.fronted@gmail.com>
2025-02-20 23:55:18 +0500

    Add promise resolve schema

--- 5. Promise/ComplexPromise/src/executable.ts
+++ 5. Promise/ComplexPromise/src/executable.ts
@@ -1,39 +1,35 @@
+export {};
 const response1 = async function foo1() {
-    const result = await fetch('https://catfact.ninja/fact');
-    return result.json()
-}
+  const result = await fetch("https://catfact.ninja/fact");
+  return result.json();
+};
 
 const response2 = async function foo2() {
-    const result = await fetch('https://catfact.ninja/fact');
-    return result.json();
-}
+  const result = await fetch("https://catfact.ninja/fact");
+  return result.json();
+};
 
-Promise.all(
-    [response1(), response2()]
-).then(value => {
-    console.log('============all')
-    console.log(value)
+Promise.all([response1(), response2()]).then((value) => {
+  console.log("============all");
+  console.log(value);
 });
 
-Promise.allSettled(
-    [response1(), response2()]
-).then(value => {
-    console.log('============allSettled')
-    console.log(value)
+const result = await Promise.all([response1(), response2()]);
+console.log("****", result);
+
+Promise.allSettled([response1(), response2()]).then((value) => {
+  console.log("============allSettled");
+  console.log(value);
 });
 
-Promise.race(
-    [response1(), response2()]
-).then(value => {
-    console.log('============race')
-    console.log(value)
+Promise.race([response1(), response2()]).then((value) => {
+  console.log("============race");
+  console.log(value);
 });
 
-Promise.any(
-    [response1(), response2()]
-).then(value => {
-    console.log('============any')
-    console.log(value)
+Promise.any([response1(), response2()]).then((value) => {
+  console.log("============any");
+  console.log(value);
 });
 
-console.log('Start program');
+console.log("Start program");
--- readme.md
+++ readme.md
@@ -1,21 +1,21 @@
 # Структура репозитория
 
 1. Общая информация о tsconfig.json и package.json
-    - разница между CJS-модулями и ESM-модулями;
-    - особенности синтаксиса;
-    - совместимость модулей различных типов;
-    - версионность ESM-модулей;
-    - использование переменных окружения в CJS-модулях и ESM-модулях.
+
+   - разница между CJS-модулями и ESM-модулями;
+   - особенности синтаксиса;
+   - совместимость модулей различных типов;
+   - версионность ESM-модулей;
+   - использование переменных окружения в CJS-модулях и ESM-модулях.
 
 2. Операторы для комплексных типов
-    - sread operator;
-    - rest operator.
-
-
+   - sread operator;
+   - rest operator.
 
 **Links**:
+
 1. Стандарт HTML5 - https://html.spec.whatwg.org/
-2. Стандарт ECMAScript - https://262.ecma-international.org/
+2. Стандарт ECMAScript - https://tc39.es/ecma262
 3. Движок V8 - https://v8.dev/
 4. Тестовая среда для Abstract syntax three - https://astexplorer.net/
 5. Документация библиотеки libuv (Node.js) - https://docs.libuv.org/
